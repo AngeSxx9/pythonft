7e1e24f3b8e1581e0d83720d5ce18470dbde32a4
409 <409@academicos.cesde.edu.co>
2023-02-18 11:53:05 -0500

    suma con python

---
+++ main.py
@@ -0,0 +1,7 @@
+numero1 = int(input('Digite un número: '))
+numero2 = int(input('Digite el segundo número: '))
+print(f'el primer numero es {numero1}')
+print(f'el segundo numero es {numero2}')
+
+suma = numero1 + numero2
+print(f'La suma de los números es: {suma}')
