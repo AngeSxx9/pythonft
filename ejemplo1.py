fb8350f740858b85bab447661feba93ecfc466c4
AngeSxx9 <anyelasofia99@gmail.com>
2023-02-25 14:20:10 -0500

    NF3 se crea archivo con condicionales sobre el nivel del agua de la Represa de Hidroituango

--- ejemplo1.py
+++ ejemplo1.py
@@ -10,6 +10,6 @@ print(f"Digite el nivel de agua: {nivelAgua}")
 if nivelAgua >= 0 and nivelAgua <= 250:
     print(f"El sensor marca {nivelAgua}, muy bajo..")
 elif nivelAgua > 250 and nivelAgua <= 400:
-    print(f"El sensor marca {nivelAgua}. Operando normal")
+    print(f"El sensor marca {nivelAgua}. NORMAL")
 else:
     print(f"El sensor marca {nivelAgua}, PELIGRO!")
